5f031793726d069e548372da167c2d7d296a365b
Chaeso99 <storymmm@naver.com>
2023-01-20 17:12:02 +0900

    23.01.20

---
+++ member/MemberBean.java
@@ -0,0 +1,46 @@
+package member;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+public class MemberBean {
+	//테이블의 컬럼
+	private int id;
+	private String name;
+	private String	phone;
+	private String address;
+	private String team;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+}
